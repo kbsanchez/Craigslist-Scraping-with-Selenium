fb10bbb249c73ad9c78b7e9ae96ee6e826821d22
kbsanchez <kbsanchez@usf.edu>
2020-08-17 11:22:03 -0400

    Search 'Honda Civic'
    updated base code to search for honda civics

--- test.py
+++ test.py
@@ -11,4 +11,10 @@ element = driver.find_element_by_xpath('//*[@id="sss0"]/li[16]/a')
 element.click()
 time.sleep(2)
 
+element = driver.find_element_by_id('query')
+element.send_keys('Honda Civic')
+time.sleep(3)
 
+element = driver.find_element_by_xpath('//*[@id="searchform"]/div[1]/button/span[1]')
+element.click()
+time.sleep(4)
