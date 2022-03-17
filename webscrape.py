3938d356330e918112c15513c1ebd92eb1cdfc70
kbsanchez <66031870+kbsanchez@users.noreply.github.com>
2022-03-17 00:03:17 -0400

    Send email with listings

--- webscrape.py
+++ webscrape.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import smtplib, ssl, getpass, io
+import email, smtplib, ssl, getpass, io
+
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -14,9 +18,14 @@ class car:
     price = ""
 
     def description(self):
-        desc_str = "Date posted: %-10s \t %-75s \t\t Price: $%-20s\n" % (self.listing_date, self.listing_title, self.price)
+        desc_str = "Date posted: {0:10} \t {1: <30} \t Price: {2:10}\n".format(self.listing_date, self.listing_title, self.price)
         return desc_str
 
+
+port = 465
+
+# Create secure SSL context
+context = ssl.create_default_context()
 
 browser = webdriver.Chrome(options=chrome_options)
 
@@ -52,6 +61,37 @@ for car in carListings:
     it = it + 1
 
 f.close()
+
+subject = f"Listings for {searchval} from Craigslist using Python"
+body = f"Here are the ten most recent listings for {searchval} from Craigslist.\n\n"
+sender_email = "devthrowaway741@gmail.com"
+password = getpass.getpass(prompt="Enter developer account password: ")
+receiver_email = input("Enter recipient email address: ")
+
+# Creating a multipart message
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+
+message.attach(MIMEText(body, "plain"))
+
+fileName = "listings.txt"
+f = open(fileName, "r")
+attachment = MIMEText(f.read())
+attachment.add_header('Content-Disposition', 'attachment', fileame=fileName)
+message.attach(attachment)
+
+signature = f"\n\nVisit github.com/kbsanchez for more cool projects!"
+
+message.attach(MIMEText(signature, "plain"))
+
+text = message.as_string()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
+    
 
 browser.quit()
 
